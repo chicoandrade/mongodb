911493c77dc6bdb6e3cc9ee44cd8be83a6298094
Francisco Andrade <francisco.andrade@ideais.com.br>
2014-10-02 18:48:24 -0300

    Acertando script de criacao de usuario

--- templates/createAdminUser.sh
+++ templates/createAdminUser.sh
@@ -4,4 +4,4 @@ export LC_ALL=C
 
 #Criando config basica para rating
 sleep 5s
-{{mongo_installation_dir}}/bin/mongo --host {{inventory_hostname}} --port {{mongo_port}} admin {{mongo_scripts_dir}}/admin_user.js
+{{mongo_installation_dir}}/{{mongo_extracted_dir_name}}/bin/mongo --port {{mongo_port}} admin {{mongo_scripts_dir}}/admin_user.js
